02f19d0d5c86d4b500f5cb52ca273f769bd9c321
Yucong Jin <yucongj@microsoft.com>
2019-02-25 00:59:34 -0800

    revert

--- generators/fabric-ui/templates/default/demo/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
-
-const root = document.createElement('div');
-document.body.appendChild(root);
-
-render(<App />, root);
