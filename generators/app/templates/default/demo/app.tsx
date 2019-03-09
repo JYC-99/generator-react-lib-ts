91f1924b82fd29f9f148bd034490e700b44d0ac9
Yucong Jin <yucongj@microsoft.com>
2019-03-08 22:25:52 -0800

    lint and demo

--- generators/app/templates/default/demo/app.tsx
+++ generators/app/templates/default/demo/app.tsx
@@ -1,22 +1,10 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
 
-import { <%= exportName %>, Functional<%= exportName %> } from '../src/index';
+import { Example } from '../src/index';
 
 const Demo = () => (
-  <div>
-    <div>
-      <%= exportName %>
-    </div>
-
-    <<%= exportName %> />
-
-    <div>
-      Functional<%= exportName %>
-    </div>
-
-    <Functional<%= exportName %> />
-  </div>
+  <Example />
 );
 
 export const App = hot(module)(Demo);
--- generators/app/templates/default/src/index.ts
+++ generators/app/templates/default/src/index.ts
@@ -1,3 +1,1 @@
-export * from './component';
-
-export * from './functional-component';
+export * from "./Example";
