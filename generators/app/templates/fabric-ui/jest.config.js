239e20b393a6e6e0c1e44d061777c7309792efa2
Yucong Jin <yucongj@microsoft.com>
2019-03-08 23:57:03 -0800

    fix lint

--- generators/app/templates/fabric-ui/jest.config.js
+++ generators/app/templates/fabric-ui/jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  transformIgnorePatterns: [
-    '/node_modules/(?!(office-ui-fabric-react))'
-  ],
   setupFiles: [
     '<rootDir>/setupTests.js'
   ],
-  snapshotSerializers: ['enzyme-to-json/serializer']
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(office-ui-fabric-react))'
+  ],
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+  ],
 };
--- generators/app/templates/fabric-ui/test/index.spec.tsx
+++ generators/app/templates/fabric-ui/test/index.spec.tsx
@@ -2,33 +2,12 @@ import 'jest';
 import * as React from 'react';
 import { shallow } from 'enzyme';
 
-import { <%= exportName %>, Functional<%= exportName %> } from '../src/index';
+import { Example } from '../src/index';
 
-describe('<%= exportName %>', () => {
+describe('Example', () => {
   it('renders correctly', () => {
-    // Do not use enzyme here. Use react-test-renderer instead
-    // It is a portal component(with ReactDom.createPortal called). Enzyme does not support it.
-    // const component = renderer.create(<<%= exportName %> />);
-    // const tree = component.toJSON();
-    // expect(tree).toMatchSnapshot();
-
     const wrapper = shallow(
-      <<%= exportName %> />,
-    );
-    expect(wrapper).toMatchSnapshot();
-  });
-});
-
-describe('Functional<%= exportName %>', () => {
-  it('renders correctly', () => {
-    // Do not use enzyme here. Use react-test-renderer instead
-    // It is a portal component(with ReactDom.createPortal called). Enzyme does not support it.
-    // const component = renderer.create(<<%= exportName %> />);
-    // const tree = component.toJSON();
-    // expect(tree).toMatchSnapshot();
-
-    const wrapper = shallow(
-      <Functional<%= exportName %> />,
+      <Example />,
     );
     expect(wrapper).toMatchSnapshot();
   });
