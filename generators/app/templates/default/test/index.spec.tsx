1179ef6837a94c06adf9b85b70c3feec261bed77
Yucong Jin <yucongj@microsoft.com>
2019-03-09 21:52:45 -0800

    hooks default

--- generators/app/templates/default/test/index.spec.tsx
+++ generators/app/templates/default/test/index.spec.tsx
@@ -2,12 +2,21 @@ import 'jest';
 import * as React from 'react';
 import { shallow } from 'enzyme';
 
-import { Example } from '../src/index';
+import { Example, SfcExample } from '../src/index';
 
 describe('Example', () => {
   it('renders correctly', () => {
     const wrapper = shallow(
       <Example />,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('SfcExample', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(
+      <SfcExample />,
     );
     expect(wrapper).toMatchSnapshot();
   });
